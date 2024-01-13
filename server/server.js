f58bfffced5809a09b519f23c44edc49e6b9a3e1
jlwood21 <jacklw21@gmail.com>
2024-01-13 11:37:50 +0000

    server updates

--- server/server.js
+++ server/server.js
@@ -11,23 +11,29 @@ app.post('/register', async (req, res) => {
     try {
         const user = new User(req.body);
         await user.save();
-        res.status(201).send({ user });
+        res.status(201).json({ user });
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).json({ error: 'Registration failed' });
     }
 });
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the SimpliTask API!');
+});
+
 
 // User login endpoint
 app.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ username: req.body.username });
         if (!user || !await bcrypt.compare(req.body.password, user.password)) {
-            return res.status(401).send({ error: 'Login failed!' });
+            return res.status(401).json({ error: 'Login failed!' });
         }
         const token = jwt.sign({ _id: user._id }, 'secretkey', { expiresIn: '2h' });
-        res.send({ user, token });
+        res.json({ user, token });
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).json({ error: 'An error occurred during login' });
     }
 });
 
